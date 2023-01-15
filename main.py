e65c00c23567d9e51cf750993ba2e2ca95f5639d
MattisBe <84279749+MattisBe@users.noreply.github.com>
2023-01-15 03:52:28 +0100

    creation_de_la_pomme

--- main.py
+++ main.py
@@ -19,12 +19,22 @@ class Jeu:
         self.serpent_direction_y = 0
         self.serpent_corps = 10
 
-
+        #créer la position de la pomme
+        self.pomme_position_x = random.randrange(110, 590, 10)
+        self.pomme_position_y = random.randrange(110, 390, 10)
+        self.pomme = 10
+        
+        #fixer les fps du jeu
+        self.clock = pg.time.Clock()
+        
+        
     def fonction_principale(self):
         #permet de gérer les événements, ainsi qu'afficher certains composants du jeu grâce aux while loop
         while self.jeu_en_cours:
             for evenement in pg.event.get():
-                print(evenement)
+                #print(evenement)
+                
+                #creation d'un événement qui permet de quitter en cliquant sur la croix 
                 if evenement.type == pg.QUIT:
                     sys.exit()
                 
@@ -32,47 +42,56 @@ class Jeu:
                 
                 if evenement.type == pg.KEYDOWN:
                     if evenement.key == pg.K_RIGHT:
-                        self.serpent_direction_x = 0.07
+                        self.serpent_direction_x = 10
                         self.serpent_direction_y = 0
-                        print("A DROITE")
+                        #print("A DROITE")
                         
                     if evenement.key == pg.K_LEFT:
-                        self.serpent_direction_x = -0.07
+                        self.serpent_direction_x = -10
                         self.serpent_direction_y = 0
-                        print("A GAUCHE")
+                        #print("A GAUCHE")
                         
                     if evenement.key == pg.K_DOWN:
-                        self.serpent_direction_y = 0.07
+                        self.serpent_direction_y = 10
                         self.serpent_direction_x = 0
-                        print("EN BAS")
+                        #print("EN BAS")
                         
                     if evenement.key == pg.K_UP:
-                        self.serpent_direction_y = -0.07
+                        self.serpent_direction_y = -10
                         self.serpent_direction_x = 0
-                        print("EN HAUT")
+                        #print("EN HAUT")
                     
+        
+            #faire bouger le serpent        
             self.serpent_position_x += self.serpent_direction_x
             self.serpent_position_y += self.serpent_direction_y
-            print(self.serpent_position_x, self.serpent_position_y)
+            #print(self.serpent_position_x, self.serpent_position_y)
             
-            
-            #faire bouger le serpent
-            
-            
+            #faire bouger la pomme si le serpent la mange
+            if self.pomme_position_y == self.serpent_position_y and self.serpent_position_x == self.pomme_position_x:
+                print("OK")
+                self.pomme_position_x = random.randrange(110, 590, 10)
+                self.pomme_position_y = random.randrange(110, 390, 10)
+                
+                
             #faire bouger le serpent si il se trouve dans les limites du jeu
             if self.serpent_position_x < 102 or self.serpent_position_x > 688 \
                 or self.serpent_position_y < 102 or self.serpent_position_y > 487:
                     sys.exit()
             
-            
+                
             self.ecran.fill((0, 0, 0))
             
             #afficher les limites 
             self.limites()
             
+            #afficher la pomme
+            pg.draw.rect(self.ecran,(255,0,0),(self.pomme_position_x, self.pomme_position_y, self.pomme, self.pomme))
+            
             #afficher le serpent
             pg.draw.rect(self.ecran, (0, 125, 0), (self.serpent_position_x, self.serpent_position_y, self.serpent_corps, self.serpent_corps))
             
+            self.clock.tick(22)
             pg.display.flip()
             
     
